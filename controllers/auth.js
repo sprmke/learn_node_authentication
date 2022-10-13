3fcfb3f33a12136f468c5e2ba0472ddcb8806852
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-13 10:05:21 +0800

    277. resetting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -115,3 +115,13 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  const errorMessage = getErrorMessage(req);
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage,
+  });
+};
